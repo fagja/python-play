14203cd9a90f33bdf60d6565b97532321364b9c6
fagja <hey.shunya19@gmail.com>
2022-09-04 16:47:24 +0900

    コンソールから1回likeはいけた

---
+++ index.js
@@ -0,0 +1,5 @@
+function tinderLike() {
+  const like_button = document.getElementsByClassName("button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a")[0];
+  like_button.click();
+}
+setTimeout(tinderLike, 10000)
